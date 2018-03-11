271371cc745063d8173c3d34d4404db2fba90951
rkschunk <rkschunk@gmail.com>
2018-03-10 22:57:22 -0300

    Update and rename aosp_sanders.mk to aosvp.mk

--- aosvp.mk
+++ aosvp.mk
@@ -18,16 +18,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 
-# Inherit some common CM stuff.
-TARGET_ARCH := arm64
-TARGET_DENSITY := xxhdpi
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_ARCORE := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AOSVP stuff.
+$(call inherit-product, vendor/aovsp/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-PRODUCT_NAME := aosp_sanders
+PRODUCT_NAME := aosvp_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
@@ -35,3 +35,6 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto G5S Plus"
+    
+#export ViperOS-AOSVP
+AOSVP_BUILD_TYPE=BETA
