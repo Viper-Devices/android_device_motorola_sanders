mendigodepijama/device_motorola_sanders-1

Add maintainer name

viper_sanders.mk
@@ -38,6 +38,10 @@ PRODUCT_NAME := viper_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
+# Maintainer name
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.viper.maintainer=marminino
+
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \

Viper-Devices/android_device_motorola_sanders

viper_sanders.mk
@@ -38,6 +38,10 @@ PRODUCT_NAME := viper_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
+# Maintainer name
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.viper.maintainer=AlissonLauffer
+
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \